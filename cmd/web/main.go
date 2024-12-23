c9554311fc85b2a7af103a8116598158c5ea84b9
Marbh56 <nathan.a.sanders2@gmail.com>
2024-12-23 18:00:46 -0500

    added path value to characterView

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Mordezzan!"))
 }
 
+func characterView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
+	w.Write([]byte(msg))
+}
+
+func characterCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/character/view/{id}", characterView)
+	mux.HandleFunc("/character/create", characterCreate)
+
 	log.Print("Starting server on :4000")
+
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
