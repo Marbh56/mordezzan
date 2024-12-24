e876379a6f5a8baa514ffbbe9968524d48435d81
Marbh56 <nathan.a.sanders2@gmail.com>
2024-12-23 20:00:49 -0500

    centralized routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,19 +21,9 @@ func main() {
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /character/view/{id}", app.characterView)
-	mux.HandleFunc("GET /character/create", app.characterCreate)
-	mux.HandleFunc("POST /character/create", app.characterCreatePost)
-
 	logger.Info("starting server", "addr", *addr)
 
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
