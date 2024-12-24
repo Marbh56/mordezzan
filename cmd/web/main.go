0769389c55db4ffa3b7eba82480e6f71e48f6e84
Marbh56 <nathan.a.sanders2@gmail.com>
2024-12-23 21:26:37 -0500

    connected localdb to app

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
+
+	"github.com/joho/godotenv"
 )
 
 type application struct {
@@ -17,13 +22,34 @@ func main() {
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
+	err := godotenv.Load()
+	if err != nil {
+		logger.Info("error loading .env file")
+		os.Exit(1)
+	}
+
+	connStr := os.Getenv("dbstring")
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		logger.Info("error opening db")
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		logger.Info("cant ping db")
+		os.Exit(1)
+	}
+
 	app := &application{
 		logger: logger,
 	}
 
 	logger.Info("starting server", "addr", *addr)
 
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
